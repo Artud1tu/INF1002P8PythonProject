fe9046b95b1a695db70aac174b59b4d70f1a06f5
dayryyl <daryltanzj@gmail.com>
2025-09-09 15:46:49 +0800

    Whitelist.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print('hello world!')
+whitelist = ['gmail.com', 'yahoo.com', 'icloud.com', 'outlook.com']
+
+print('Welcome to safe email checker, please input your email')
+
+emails_to_check = []
+user_email = input('Enter your email: ')
+emails_to_check.append(user_email.lower())
+
+for email in emails_to_check:
+       if "@" not in email or email.count("@") != 1:
+            print(f'{email} invalid email format')
+       elif " " in email:
+            print(f'{email} cannot contain space')
+       elif email.endswith("@"):
+            print(f'{email} missing domain')   
+       else:
+             safe_domain = email.split('@')[1]
+             if safe_domain in whitelist:
+                print(f'{email} is safe')
+             else:
+                print(f'{email} is not safe')
