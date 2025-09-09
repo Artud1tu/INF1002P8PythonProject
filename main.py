b062bd949a48419b4046ae9d4390e100704d7b02
Artud1tu <stanley_han_619hotmail.com>
2025-09-09 15:41:19 +0800

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Main python file for project
+# whitelist
+SAFE_DOMAINS = ["example.com", "myschool.edu"]
+
+def safe_domain(sender_email: str) -> bool:
+    domain = sender_email.split("@")[-1].lower()
+    return domain in SAFE_DOMAINS
+
+def main():
+    test_email = {"from": "test@g00gle.com", "subject": "Urgent! Verify your account"}
+
+    print("Checking email:", test_email)
+    if safe_domain(test_email["from"]):
+        print("Safe Email")
+    else:
+        print("Suspicious Email")
+
+if __name__ == "__main__":
+    main()
